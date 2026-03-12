99891249160769f81fca648601a79034fc1ba055
kubernet <kubernet@master.labs.local>
2026-03-12 21:06:18 +0900

    fix: auth/templates/auth/default.html

--- blueprints/study/__init__.py
+++ blueprints/study/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
 # diary 블루프린트 생성
-diary_bp = Blueprint('study', __name__, template_folder='templates/study')
+diary_bp = Blueprint('study', __name__, template_folder='templates')
 
 from . import routes
